2049618696c7a97696c304d89a3a10be0f3fb809
Jaqueline.Brandão <jaqueline.brandao@prodeb.ba.gov.br>
2015-06-18 13:14:22 -0300

    first
    first commit
    erro somente no teste

---
+++ app/controllers/Application.java
@@ -0,0 +1,26 @@
+package controllers;
+
+import play.*;
+import play.data.validation.Required;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class Application extends Controller {
+
+    public static void index() {
+        render();
+    }
+   
+    public static void sayHello(String myName) {
+    	 if(validation.hasErrors()) {
+    	        flash.error("Oops, please enter your name!");
+    	        index();
+    	    }
+    	render(myName);
+    }
+}
+    
+ 
